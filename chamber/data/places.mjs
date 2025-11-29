a7770c4ea3a84ef3aef9decfcd2a4f04ed147814
Abdulsomad <abdulsalamabdulsomad14@gmail.com>
2025-11-29 17:10:26 +0100

    Add solutions to tasks

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -1,49 +1,49 @@
 export const places = [
     {
         "name": "Sobi Hill",
-        "image": "images/sobi_hill.jpeg",
+        "image": "images/sobi_hill.webp",
         "address": "Sobi, Ilorin, Kwara State",
         "description": "One of the highest natural hills in Ilorin offering scenic views and a significant spiritual history."
     },
     {
         "name": "Ilorin Central Mosque",
-        "image": "images/central_mosque.jpeg",
+        "image": "images/central_mosque.webp",
         "address": "Emir's Road, Ilorin",
         "description": "A stunning architectural landmark and one of the largest mosques in Nigeria, recognized for its golden domes."
     },
     {
         "name": "Esie Museum",
-        "image": "images/esie_museum.jpeg",
+        "image": "images/esie_museum.webp",
         "address": "Esie Town, Near Ilorin",
         "description": "Nigeria’s first museum, housing over 800 mysterious stone figures believed to be centuries old."
     },
     {
         "name": "Kwara State Stadium",
-        "image": "images/kwara_stadium.jpeg",
+        "image": "images/kwara_stadium.webp",
         "address": "Stadium Road, Ilorin",
         "description": "A major sports complex that hosts national competitions, football matches, and community events."
     },
     {
         "name": "University of Ilorin Zoo",
-        "image": "images/unilorin_zoo.jpeg",
+        "image": "images/unilorin_zoo.webp",
         "address": "University of Ilorin, Tanke",
         "description": "A popular attraction featuring diverse wildlife, nature trails, and student conservation activities."
     },
     {
         "name": "Shoprite Mall Ilorin",
-        "image": "images/shoprite_ilorin.jpeg",
+        "image": "images/shoprite_ilorin.webp",
         "address": "Fate Road, GRA Ilorin",
         "description": "A bustling shopping center with retail stores, eateries, entertainment, and community activities."
     },
     {
         "name": "Dada Pottery",
-        "image": "images/dada_pottery.jpeg",
+        "image": "images/dada_pottery.webp",
         "address": "Dada Area, Okelele, Ilorin",
         "description": "A historic pottery village known for handcrafted clay art created by generations of local artisans."
     },
     {
         "name": "Owu Waterfalls",
-        "image": "images/owu_waterfalls.jpeg",
+        "image": "images/owu_waterfalls.webp",
         "address": "Owa-Onire, Near Ilorin",
         "description": "One of the highest waterfalls in West Africa, offering a refreshing escape and breathtaking scenery."
     }
